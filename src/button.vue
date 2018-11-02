21ee83e108ee0ad589532f377ad70f6a48bc650b
godheaven_007 <345777292@qq.com>
2018-11-02 11:20:12 +0800

    parcel构建vue单文件组件

---
+++ src/button.vue
@@ -0,0 +1,33 @@
+<template>
+    <button class="g-button">{{msg}}</button>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                msg: '按钮'
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .g-button {
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--border-raidus);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+        &:active {
+            background: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
+    }
+</style>
