915befbbfa88b0a8e6b3cf9f3ac14d47e78430b8
godheaven007 <345777292@qq.com>
2018-11-05 14:06:21 +0800

    g-button-group创建

---
+++ src/button-group.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="g-button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss">
+    .g-button-group {
+        display: flex;
+        > .g-button {
+            position: relative;
+            border-radius: 0;
+            margin-left: -1px;
+            &:first-child {
+                border-top-left-radius: var(--border-raidus);
+                border-bottom-left-radius: var(--border-raidus);
+            }
+            &:last-child {
+                border-top-right-radius: var(--border-raidus);
+                border-bottom-right-radius: var(--border-raidus);
+            }
+            &:hover{
+                z-index: 1;
+            }
+        }
+    }
+</style>
